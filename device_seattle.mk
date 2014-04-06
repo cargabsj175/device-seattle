4759f41a39e6318bf6ef79bd275dffb3b378da96
Carlos Sánchez <cargabsj175@gmail.com>
2014-04-06 17:53:38 -0430

    merging cm.mk into device_seattle.mk

--- device_seattle.mk
+++ device_seattle.mk
@@ -1,5 +1,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+# Release name
+PRODUCT_RELEASE_NAME := seattle
+
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
@@ -15,10 +18,13 @@ else
 endif
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):prebuilt/zImage
+    $(LOCAL_KERNEL):prebuilt/kernel
 
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_seattle
 PRODUCT_DEVICE := seattle
+PRODUCT_BRAND := qcom
+PRODUCT_MANUFACTURER := siragon
+PRODUCT_MODEL := sp-5000
