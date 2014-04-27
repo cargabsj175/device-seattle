1cf40bfb4fba9483403a9d9cde6921350867ef6f
Carlos Sanchez <cargabsj175@gmail.com>
2014-04-27 11:11:10 -0430

    added recovery & filesistem stuff.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,9 +28,21 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := seattle
 
+TARGET_PREBUILT_KERNEL := device/siragon/seattle/prebuilt/kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom loglevel=1
 BOARD_KERNEL_BASE := 0x00200000
 BOARD_KERNEL_PAGESIZE := 4096
+
+# Recovery
+ENABLE_LIBRECOVERY := true
+
+# librecovery
+RECOVERY_EXTERNAL_STORAGE := /emmc
+
+# filesystem
+SYSTEM_FS_TYPE            := ext4
+SYSTEM_PARTITION_TYPE     := EMMC
+SYSTEM_LOCATION           := /dev/block/mmcblk0p12
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
@@ -41,7 +53,5 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 # hack to prevent llvm from building
 BOARD_USE_QCOM_LLVM_CLANG_RS := true
-
-TARGET_PREBUILT_KERNEL := device/siragon/seattle/prebuilt/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
